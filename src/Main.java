08701590eaa7bd1f3b148832b320eb64bd70067a
alstonwang <wanga23@wfu.edu>
2025-01-29 22:05:46 -0500

    Fixed the printReceiptInOrder method and added if-else statements to make the spacing look good.

--- src/Main.java
+++ src/Main.java
@@ -25,12 +25,13 @@ public class Main {
             n = Integer.parseInt(args[0]);
         } catch (NumberFormatException e1) {
             System.out.println("\"" + args[0] + " is not a valid integer.");
+            System.exit(1);
         }
         ArrayList<Item> cart = new ArrayList<>(n);
         int[] index = new int[n];
         for (int i = 0; i < n; i++) {
             try {
-                index[i] = Integer.parseInt(args[i]);
+                index[i] = Integer.parseInt(args[i + 1]);
                 cart.add(store[index[i]]);
             } catch (NumberFormatException e2) {
                 System.out.println("\"" + n + " is not a valid integer.");
@@ -43,11 +44,27 @@ public class Main {
 
     public static void printReceiptInOrder(ArrayList<Item> cart, Item[] store) {
         System.out.println("Receipt");
-        System.out.println("=========================");
-        System.out.println("Item                Price");
+        System.out.println("============================");
+        System.out.println("Item                   Price");
         int size = cart.size();
+        double subtotal = 0;
         for (int i = 0; i < size; i++) {
-
+            String itemName = cart.get(i).getItemName();
+            if (itemName.equals("iPhone 16 Pro") || itemName.equals("AirPods Pro 2")) {
+                System.out.println(itemName + "          " + cart.get(i).getItemPrice());
+            } else if (itemName.equals("Apple Watch Ultra 2")) {
+                System.out.println(itemName + "    " + cart.get(i).getItemPrice());
+            } else if (itemName.equals("MacBook Pro")) {
+                System.out.println(itemName + "            " + cart.get(i).getItemPrice());
+            } else if (itemName.equals("iPad Pro")) {
+                System.out.println(itemName + "               " + cart.get(i).getItemPrice());
+            }
+            subtotal += cart.get(i).getItemPrice();
         }
+        System.out.println("============================");
+        double total = subtotal * 1.05;
+        System.out.printf("(a) Subtotal: %.2f\n", subtotal);
+        System.out.println("(b) Sales Tax: 5%");
+        System.out.printf("(c) Total: %.2f\n", total);
     }
 }
