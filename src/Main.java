3756bc78c125a95fe7528b7a3a2c5e245b91d507
alstonwang <wanga23@wfu.edu>
2025-01-30 21:41:26 -0500

    Completed all tasks, but have to check for more exceptions and answer the question about running times.

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,8 @@ public class Main {
     public static void main(String[] args) {
         Item[] store = setupStore();
         ArrayList<Item> cart = createCart(args, store);
-        printReceiptInOrder(cart, store);
+        printReceiptInOrder(cart);
+        emptyCartReverseOrder(cart);
     }
 
     public static Item[] setupStore() {
@@ -21,28 +22,36 @@ public class Main {
 
     public static ArrayList<Item> createCart(String[] args, Item[] store) {
         int n = 0;
+        int limit = args.length;
         try {
             n = Integer.parseInt(args[0]);
         } catch (NumberFormatException e1) {
-            System.out.println("\"" + args[0] + " is not a valid integer.");
+            System.out.println("\"" + args[0] + "\" is not a valid integer.");
             System.exit(1);
         }
         ArrayList<Item> cart = new ArrayList<>(n);
-        int[] index = new int[n];
-        for (int i = 0; i < n; i++) {
-            try {
-                index[i] = Integer.parseInt(args[i + 1]);
-                cart.add(store[index[i]]);
-            } catch (NumberFormatException e2) {
-                System.out.println("\"" + n + " is not a valid integer.");
-            } catch (ArrayIndexOutOfBoundsException e3) {
-                System.out.println("The store does not have an item of index " + args[0]);
+        if (limit == 1) {
+            System.out.println("No valid input to cart.");
+            System.exit(1);
+        } else {
+            for (int i = 1; i < limit; i++) {
+                try {
+                    cart.add(store[Integer.parseInt(args[i])]);
+                } catch (NumberFormatException e2) {
+                    System.out.println("\"" + args[i] + "\" is not a valid integer.");
+                    if (i == limit - 1)
+                        System.exit(1);
+                } catch (ArrayIndexOutOfBoundsException e3) {
+                    System.out.println("The store does not have an item of index " + Integer.parseInt(args[i]) + ".");
+                    if (i == limit - 1)
+                        System.exit(1);
+                }
             }
         }
         return cart;
     }
 
-    public static void printReceiptInOrder(ArrayList<Item> cart, Item[] store) {
+    public static void printReceiptInOrder(ArrayList<Item> cart) {
         System.out.println("Receipt");
         System.out.println("============================");
         System.out.println("Item                   Price");
@@ -50,14 +59,20 @@ public class Main {
         double subtotal = 0;
         for (int i = 0; i < size; i++) {
             String itemName = cart.get(i).getItemName();
-            if (itemName.equals("iPhone 16 Pro") || itemName.equals("AirPods Pro 2")) {
-                System.out.println(itemName + "          " + cart.get(i).getItemPrice());
-            } else if (itemName.equals("Apple Watch Ultra 2")) {
-                System.out.println(itemName + "    " + cart.get(i).getItemPrice());
-            } else if (itemName.equals("MacBook Pro")) {
-                System.out.println(itemName + "            " + cart.get(i).getItemPrice());
-            } else if (itemName.equals("iPad Pro")) {
-                System.out.println(itemName + "               " + cart.get(i).getItemPrice());
+            switch (itemName) {
+                case "iPhone 16 Pro":
+                case "AirPods Pro 2":
+                    System.out.println(itemName + "          " + cart.get(i).getItemPrice());
+                    break;
+                case "Apple Watch Ultra 2":
+                    System.out.println(itemName + "    " + cart.get(i).getItemPrice());
+                    break;
+                case "MacBook Pro":
+                    System.out.println(itemName + "            " + cart.get(i).getItemPrice());
+                    break;
+                case "iPad Pro":
+                    System.out.println(itemName + "               " + cart.get(i).getItemPrice());
+                    break;
             }
             subtotal += cart.get(i).getItemPrice();
         }
@@ -66,5 +81,15 @@ public class Main {
         System.out.printf("(a) Subtotal: %.2f\n", subtotal);
         System.out.println("(b) Sales Tax: 5%");
         System.out.printf("(c) Total: %.2f\n", total);
+    }
+
+    public static void emptyCartReverseOrder(ArrayList<Item> cart) {
+        System.out.println("Removing all items from the cart in \"Last In First Out\" order");
+        int index = cart.size() - 1;
+        for (int i = index; i >= 0; i--) {
+            System.out.println("Removing: " + cart.get(i).getItemName());
+            cart.remove(i);
+        }
+        System.out.println("Cart has been emptied");
     }
 }
